f57ab837faee31f64b31beda7062635ab45f8755
smuerdel <95031292+smuerdel@users.noreply.github.com>
2023-03-25 08:23:43 +0100

    erased info

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-insert into users (username, firstname, surname, age, email, create_date) values ('smuerdel', 'Samuel', 'Mürdel', 26, 's.chipsfrisch@web.de', '2023-01-01 00:00:00')
+insert into users (username, firstname, surname, age, email, create_date) values ('smuerdel', 'Samuel', 'Mürdel', 26, 'samuel@web.de', '2023-01-01 00:00:00')
 insert into users (username, firstname, surname, age, email, create_date) values ('eins', 'Heins', '', 70, 'azuresql@brathuhn.de', '2023-01-01 00:00:00')
 insert into users (username, firstname, surname, age, email, create_date) values ('zwei', 'Zweins', '', 5, 'api@brathuhn.de', '2023-01-01 00:00:00')
 insert into users (username, firstname, surname, age, email, create_date) values ('drei', 'Dreist', '', 45, 'info@brathuhn.de', '2023-01-01 00:00:00')
